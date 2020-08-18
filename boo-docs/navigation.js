789e4d21c2da7d836dcba17d9da545747dffafd5
Charlie MacFadyen <csmacf@gmail.com>
2020-08-17 21:14:25 -0400

    Bootstrap

--- boo-docs/navigation.js
+++ boo-docs/navigation.js
@@ -5,35 +5,15 @@ $(document).ready(function(){
   console.log(chapter);
   //add chapter number to top div
   $('div.title-header').append(" - Chapter " + chapter);
-  //create buttons
-  const indexbutton = '<a class="btn btn-primary" id="index-button" href="#" >Index</a>';
-  //<button type="button" id="index-button" >Index</button>';
-  const nextbutton = '<button type="button" id="next-button" >Next</button>';
-  const prevbutton = '<button type="button" id="prev-button" >Previous</button>';
 
-  //add to page
-$('div.chap-nav').append(prevbutton);
-$('div.chap-nav').append(indexbutton);
-$('div.chap-nav').append(nextbutton);
 
 if(chapter < 2){
-  $('#prev-button').css("opacity", "0.5");
+  $('#prev-btn').addClass("disabled");
+  $('#prev-btn').attr('aria-disabled',"true");
 }
   //add links
-  $("#index-button".attr('href', 'http://www.nyt.com/');
-
-
-
-
-$( "#next-button" ).click(function() {
-
-  window.location.href = ("Ch" + (chapter + 1) + ".html");
-});
-
-$( "#prev-button" ).click(function() {
-
-  window.location.href = ("Ch" + (chapter - 1) + ".html");
-
-});
+  $('#index-btn').attr('href', 'https://www.nyt.com/');
+  $('#prev-btn').attr('href', 'Ch' + (chapter-1) + '.html');
+  $('#next-btn').attr('href', 'Ch' + (chapter+1) + '.html');
 
 });
